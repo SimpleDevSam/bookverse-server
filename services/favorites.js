734f25f78b7541b84aa92aa4ee17a8a2749ecd2f
Samuel <samuelufop121035@gmail.com>
2023-06-13 14:25:07 -0300

    prechanges

--- services/favorites.js
+++ services/favorites.js
@@ -10,7 +10,31 @@ function deleteFavoriteById(id) {
     fs.writeFileSync("favbooksdb.json", JSON.stringify(filteredBooks))
 }
 
-function insertFavorite (id) {
+function insertFavorite(id) {
+    const books = JSON.parse(fs.readFileSync("booksdb.json"));
+    const favorites = JSON.parse(fs.readFileSync("favbooksdb.json"));
+  
+    const insertedBook = books.find((book) => book.id === id);
+  
+    if (!insertedBook) {
+      throw new Error("Livro não encontrado.");
+    }
+  
+    const isBookInFavorites = favorites.some((book) => book.id === id);
+  
+    if (isBookInFavorites) {
+      throw new Error("Livro já existe na lista de favoritos.");
+    }
+  
+    const newFavoritesList = [...favorites, insertedBook];
+  
+    fs.writeFileSync("favbooksdb.json", JSON.stringify(newFavoritesList));
+    return "Livro adicionado à lista de favoritos.";
+  }
+  
+  
+
+/*function insertFavorite (id) {
     const books = JSON.parse(fs.readFileSync("booksdb.json"))
     const favorites = JSON.parse(fs.readFileSync("favbooksdb.json"))
 
@@ -18,7 +42,7 @@ function insertFavorite (id) {
     const newFavoritesList = [...favorites, insertedBook]
 
     fs.writeFileSync("favbooksdb.json", JSON.stringify(newFavoritesList))
-}
+}*/
 
 module.exports = {
     getAllFavorites,
